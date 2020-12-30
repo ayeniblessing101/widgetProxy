e6e4be949d6723628219fd6ec93eee4dd00b24b1
Blessing Ayeni <blessingayeni@Blessings-MacBook-Pro.local>
2020-12-30 20:15:05 +0100

    enable cors preflight

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const express = require("express");
-const cors = require("cors");
 const weather = require("./weather.js");
 
 const app = express();
 const port = process.env.PORT || 8080;
 
 app.use(express.json());
-app.options("*", cors());
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 app.get("/weather", (req, res) => {
   const location = req.query.location;
