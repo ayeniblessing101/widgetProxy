c0ecc96ddeef4fce18632322299d62cad89e8b02
Blessing Ayeni <blessingayeni@Blessings-MacBook-Pro.local>
2020-12-30 15:23:24 +0100

    enable cors preflight

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-const { request } = require("express");
 const express = require("express");
+const cors = require("cors");
 const weather = require("./weather.js");
 
 const app = express();
 const port = process.env.PORT || 8080;
 
 app.use(express.json());
+app.options("*", cors());
 
 app.get("/weather", (req, res) => {
   const location = req.query.location;
